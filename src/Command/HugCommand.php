ff7ec6217e5afe63673dbd99915232668a42bf64
hugodealmeida <hugo.dealmeida@lephare.com>
2020-06-08 17:15:18 +0200

    Ajout de l'option --ansible-path avec parse du fichier hosts de Ansible

--- src/Command/HugCommand.php
+++ src/Command/HugCommand.php
@@ -5,6 +5,7 @@ namespace Hug\Command;
 use Hug\Service\ParseJson;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class HugCommand extends Command
@@ -24,6 +25,12 @@ class HugCommand extends Command
         $this
             ->setDescription("(TEST CONFIGURATION) - Génère un fichier goss_projet.yaml à partir d'un composer.json")
             ->setHelp('Récupère le composer.json à la racine du projet et viens générer un fichier goss_projet.yaml pour venir le tester par la suite avec Goss')
+            ->addOption(
+                'ansible-path',
+                'anspath',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                "Récupérer les variables d'environnement Ansible du projet"
+            )
 
         ;
     }
@@ -33,9 +40,9 @@ class HugCommand extends Command
         $output->writeln([
         'Génération du goss_projet.yaml',
         '==============================',
-        '',
     ]);
-        $this->parseJson->ParseJson();
+        $ansible = $input->getOption('ansible-path');
+        $this->parseJson->ParseJson($ansible);
         // TODO: si fichier écrasé, le précisé dans les logs
         return Command::SUCCESS;
     }
--- src/Service/ParseJson.php
+++ src/Service/ParseJson.php
@@ -6,25 +6,39 @@ use Symfony\Component\Yaml\Yaml;
 
 class ParseJson
 {
-    public function ParseJson(): void
+    public function ParseJson(array $ansible): void
     {
         $json = file_get_contents('./tests/composer.json');
         $json_data = json_decode($json, true);
         $extensions = $json_data['require'];
         $keys = array_keys($extensions);
         $pattern = '/^ext-/i';
-        $yaml = preg_grep($pattern, $keys);
-        $value = Yaml::parseFile('./tests/TestTemplate.yaml');
+        $array = preg_grep($pattern, $keys);
+        $yaml = array_values($array);
+        //récupérer URL selon chemin Ansible
+        $temp = array_values($ansible);
+        $string = implode('', $temp);
+        $url = file_get_contents($string, false, null, 5); //récupère la valeur après [app] => pas très propre
+        print_r($url);
 
         if (file_exists('./fichierGoss/goss_projet.yaml')) {
             echo 'Le fichier existe déjà, il va être supprimé';
-            $template = Yaml::dump($value);
+            $template = Yaml::dump([
+                'http' => [$url => ['status' => 200, 'body' => null]],
+            ], 4, 2);
             file_put_contents('./fichierGoss/goss_projet.yaml', $template);
-            file_put_contents('fichierGoss/goss_projet.yaml', $yaml, FILE_APPEND);
+            $phpExtensions = Yaml::dump($yaml, 2, 2, 0);
+            file_put_contents('fichierGoss/goss_projet.yaml', $phpExtensions, FILE_APPEND);
         } else {
-            $template = Yaml::dump($value);
+            $template = Yaml::dump([
+                'http' => ['http://proxibleus.hde.lph/test_projet.php' => ['status' => 200, 'body' => null]],
+            ], 4, 2);
             file_put_contents('./fichierGoss/goss_projet.yaml', $template);
-            file_put_contents('fichierGoss/goss_projet.yaml', $yaml, FILE_APPEND);
+            $phpExtensions = Yaml::dump($yaml, 2, 2, 0);
+            file_put_contents('fichierGoss/goss_projet.yaml', $phpExtensions, FILE_APPEND);
         }
     }
 }
+
+// parser la conf ansible, ajouter une option --ansible-path pour récupérer un dossier externe à /hugo
+// fichier hosts dans ansible, se renseigner sur la config ansible du phare ou conf général
